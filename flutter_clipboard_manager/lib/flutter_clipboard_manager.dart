5f2b87ebcfb930868ca8ef77b7446ac9f39699ac
Rosenberg, Peter <peter.1.rosenberg@here.com>
2021-05-19 23:35:34 +0200

    Migrate to null safety

--- flutter_clipboard_manager/lib/flutter_clipboard_manager.dart
+++ flutter_clipboard_manager/lib/flutter_clipboard_manager.dart
@@ -6,8 +6,8 @@ import 'package:flutter_clipboard_manager_platform_interface/flutter_clipboard_m
 class FlutterClipboardManager {
   /// Copies [text] to the clipboard and returns whether
   /// the operation was successful or not.
-  static Future<bool> copyToClipBoard(String text) => FlutterClipboardManagerPlatform.instance.copyToClipBoard(text);
+  static Future<bool?> copyToClipBoard(String text) => FlutterClipboardManagerPlatform.instance.copyToClipBoard(text);
 
   /// Returns the current clipboard contents.
-  static Future<String> copyFromClipBoard() => FlutterClipboardManagerPlatform.instance.copyFromClipBoard();
+  static Future<String?> copyFromClipBoard() => FlutterClipboardManagerPlatform.instance.copyFromClipBoard();
 }
